95126cfebe685caa46d09e076649fb5689440f11
darkgrecher <hashanrajakaruna123456@gmail.com>
2025-12-17 18:20:13 +0530

    style: Standardize quotation marks in metro.config.js

--- mobile-app/metro.config.js
+++ mobile-app/metro.config.js
@@ -1,11 +1,11 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
 // Resolve import.meta issues for web
 config.resolver = {
   ...config.resolver,
-  sourceExts: [...(config.resolver?.sourceExts || []), 'mjs'],
+  sourceExts: [...(config.resolver?.sourceExts || []), "mjs"],
 };
 
 module.exports = config;
